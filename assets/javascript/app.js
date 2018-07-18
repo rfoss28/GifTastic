3117cb0112c7facf49035badf6ffc48eff917456
Ryan <rfoss28@comcast.net>
2018-07-17 20:55:21 -0400

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
   var topics = ["duck", "cat", "dog", "goose"];
 
-  //loops through each animal and creates  button for it
-    topics.forEach(function(animal) {
+  createButtons();
+
+
+  function createButtons(){
+
+  $("#buttons-view").empty();
+    //loops through each animal and creates button for it
+    
+  topics.forEach(function(animal) {
       console.log(animal);
       var a = $("<button>");
       a.text(animal);
       $("#buttons-view").append(a);
     });
+  }
+
+  $("#add-animal").on("click", function(event){
+   event.preventDefault();
+// captures the animal entered in the text box
+    var newAnimal = $("#animal-input").val();
+    console.log(newAnimal);
+    // adds the user entered animal to the array
+    topics.push(newAnimal);
+    createButtons();
+
+  });
+
 });
